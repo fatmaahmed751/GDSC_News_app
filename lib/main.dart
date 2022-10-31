17ed26913a833c04c2fa8b231de84af6a4dd0fb6
pc <fatmaahmed0008@gmail.com>
2022-10-31 02:36:19 +0200

    add changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gdscnews_app/core/services/services_locator.dart';
+import 'package:gdscnews_app/presentation/controller/news_events.dart';
+import 'presentation/controller/news_bloc.dart';
+import 'presentation/screens/news_home_screen.dart';
 
 void main() {
-
-
-  runApp(const MyApp());
+  ServiceLocator().init();
+  // WidgetsFlutterBinding.ensureInitialized();
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-
+    return MultiBlocProvider(
+      providers:[
+        BlocProvider(create: (context) =>
+        NewsBloc(sl(),sl(),sl())..add(GetNewsByIdEvent())..add(GetPoliticsNewsEvent())..add(GetScienceNewsEvent()),
+        )],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: NewsLayout(),
+      ),
     );
   }
 }
